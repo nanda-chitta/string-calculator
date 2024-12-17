104e82f56ff7bdd82daa2fcd60a2509ff0d5a407
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2024-12-17 20:51:16 +0530

    Optimized the code changes

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,13 +3,17 @@
 module StringCalculator
   def add(str)
     return 0 if str.empty?
-    raise if str.include?('-')
 
+    raise_if_negative(str)
     num = numbers(str)
     num.inject(:+)
   end
 
   def numbers(str)
     str.scan(/-?\d+/).map(&:to_i)
+  end
+
+  def raise_if_negative(str)
+    raise if str.include?('-')
   end
 end
