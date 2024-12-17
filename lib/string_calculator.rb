ccf867da5b5d600376e9d6fcc8fe9a638e057f10
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2024-12-17 20:48:58 +0530

    Succes: after added to raise exception

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,12 +3,13 @@
 module StringCalculator
   def add(str)
     return 0 if str.empty?
+    raise if str.include?('-')
 
     num = numbers(str)
     num.inject(:+)
   end
 
   def numbers(str)
-    str.scan(/\d+/).map(&:to_i)
+    str.scan(/-?\d+/).map(&:to_i)
   end
 end
