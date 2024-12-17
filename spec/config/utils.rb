ae4d02f5d5f31ce22381ae7bb493595ead9b542a
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2024-12-17 16:39:36 +0530

    Success: after added StringCalculator module

---
+++ spec/config/utils.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'string_calculator'
+
+RSpec.configure do |_config|
+  include StringCalculator
+end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'config/utils'
+
 RSpec.describe StringCalculator, type: :module do
   describe 'string_calculator' do
     it 'return should defined as "constant"' do
