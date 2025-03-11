b2a3054a7f7ba2971bd11aa6f5cc26c0ed4d1160
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2025-03-11 16:10:41 +0530

    Added: optimized the code

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,9 +4,13 @@ module StringCalculator
   def add(str)
     return 0 if str.empty?
 
-    digits = str.split(',').map(&:to_i)
+    digits = numbers(str)
     return digits[0] + digits[1] if str.include?(',')
 
     str.to_i
+  end
+
+  def numbers(str)
+    str.split(',').map(&:to_i)
   end
 end
