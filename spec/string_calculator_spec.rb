253bc4cf2679c040d92c2e3479254a8a83908f7a
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2025-03-11 18:09:21 +0530

    Added: test for multiple custom delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -72,6 +72,18 @@ RSpec.describe StringCalculator, type: :module do
       it 'should allow the semicolon' do
         expect(add("//;\n1;2;3")).to eq(6)
       end
+
+      it 'should allow the letter' do
+        expect(add('//x52$55@51X14')).to eq(172)
+      end
+
+      it 'should allow the special delimiter' do
+        expect(add('//[***]\n1***2***3')).to eq(6)
+      end
+
+      it 'should allow the multiple delimiter' do
+        expect(add('//[*][%]\n1*2%3')).to eq(6)
+      end
     end
   end
 end
