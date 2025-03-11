77e263dd8ece51b502ae6fe92aedd3588fe29510
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2025-03-11 13:33:47 +0530

    Error: ArgumentError for add method.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,5 +13,9 @@ RSpec.describe StringCalculator, type: :module do
     it 'should respond_to method' do
       expect(StringCalculator.respond_to?(:add)).to eq(true)
     end
+
+    it 'should return 0 for empty string' do
+      expect(add('')).to eq(0)
+    end
   end
 end
