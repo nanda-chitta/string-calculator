a6427243d48ee71530da313c42956a40236a08ee
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2025-03-11 18:19:29 +0530

    Added: Optimized code for raise exception for negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,13 +3,17 @@
 module StringCalculator
   def add(str)
     return 0 if str.empty?
-    raise if str.include?('-')
 
+    raise_if_negative(str)
     digits = numbers(str)
     digits.reduce { |sum, digit| sum + digit }
   end
 
   def numbers(str)
     str.scan(/-?\d+/).map(&:to_i)
+  end
+
+  def raise_if_negative(str)
+    raise if str.include?('-')
   end
 end
