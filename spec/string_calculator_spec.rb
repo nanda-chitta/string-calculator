3837fd9e635ad1fbfe59323d78aa7d46d9e4c716
Chitta Ranjan Nanda <ranjanchitta.nanda@gmail.com>
2025-03-11 14:54:34 +0530

    Filure: argument not matched

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -22,6 +22,14 @@ RSpec.describe StringCalculator, type: :module do
       it 'return 3 if pass "3" string as argument' do
         expect(add('3')).to eq(3)
       end
+
+      it 'return 5 if pass "5" as argument' do
+        expect(add('5')).to eq(5)
+      end
+
+      it 'return 25 if pass "25" as argument' do
+        expect(add('25')).to eq(25)
+      end
     end
   end
 end
